d2d40a4fdce650e30745af2561262d56e69b1e12
chakra <chakrabhandari@gmail.com>
2019-01-17 15:29:33 +1100

    Create types.js

---
+++ types/types.js
@@ -0,0 +1,25 @@
+export const ADD_TRANSACTION = 'ADD_TRANSACTION';
+export const EDIT_TRANSACTION = 'EDIT_TRANSACTION';
+export const READ_TRANSACTION = 'READ_TRANSACTION';
+export const ARCHIVE_TRANSACTION = 'ARCHIVE_TRANSACTION';
+
+export const LIST_TRANSACTIONS = 'ADD_TRANSACTION';
+
+export const ADD_CONTACT = 'ADD_CONTACT';
+export const EDIT_CONTACT = 'EDIT_CONTACT';
+export const READ_CONTACT = 'READ_CONTACT';
+export const LIST_CONTACTS = 'LIST_CONTACTS';
+export const DELETE_CONTACT = 'DELETE_CONTACT';
+
+export const GENERATE_QUOTE = 'GENERATE_QUOTE';
+
+export const GET_RATE = 'GET_RATE';
+
+export const ADD_CLIENT = 'ADD_CLIENT';
+export const EDIT_CLIENT = 'EDIT_CLIENT';
+export const LIST_CLIENTS = 'LIST_CLIENTS';
+export const READ_CLIENT = 'READ_CLIENT';
+
+
+
+
