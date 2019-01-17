c53dfffd335fa7485ce4329aa794b332244225ca
chakra <chakrabhandari@gmail.com>
2019-01-17 15:32:25 +1100

    Update types.js

--- types/types.js
+++ types/types.js
@@ -21,5 +21,14 @@ export const LIST_CLIENTS = 'LIST_CLIENTS';
 export const READ_CLIENT = 'READ_CLIENT';
 
 
+export const GET_TOKEN = 'GET_TOKEN';
+export const SAVE_TOKEN = 'SAVE_TOKEN';
+export const REMOVE_TOKEN = 'REMOVE_TOKEN';
+
+export const LOADING = 'LOADING';
+export const ERROR = 'ERROR';
+
+
+
 
 
